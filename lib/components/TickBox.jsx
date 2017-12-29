44dc39d4bef686e6133f060eb9855eed2f40af8c
Dineshraj Goomany <dineshraj.goomany@bbc.co.uk>
2017-12-29 17:58:12 +0000

    makes TickBox stateless

--- lib/components/TickBox.jsx
+++ lib/components/TickBox.jsx
@@ -1,38 +1,17 @@
 import React from 'react';
 import Label from './Label';
 
-class TickBox extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      checked: this.props.checked,
-    };
-  }
-
-  _toggleCheckedValue() {
-    this.setState({
-      checked: !this.state.checked
-    }, () => this.props.clickCallback(this.state.checked));
-  }
-
-  _isChecked() {
-
-    return this.state.checked ? ' checked' : '';
-  }
-
-  render() {
-    const labelClass = this._isChecked();
-    const isChecked = this._isChecked();
-    const id = this.props.id;
-
-    return (
-      <div className="tick-box">
-        <Label id={id} label={this.props.label} additionalClass={labelClass} />
-        <input className="tick-box__input" id={id} name={id} type="checkbox" value={id} onClick={this._toggleCheckedValue.bind(this)} checked={isChecked} />
-      </div>
-    );
-  }
+function _toggleCheckedValue(e) {
+  this.props.clickCallback(e.target.checked);
 }
 
-module.exports = TickBox;
+export default ({ id, label, checked }) => {
+  const isChecked = checked ? 'checked': '';
+
+  return (
+    <div className="tick-box">
+      <Label id={id} label={label} additionalClass={isChecked} />
+      <input className="tick-box__input" id={id} name={id} type="checkbox" value={id} onChange={(e) => _toggleCheckedValue(e)} defaultChecked={isChecked} />
+    </div>
+  );
+};
