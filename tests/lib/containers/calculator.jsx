dab3e99a6e5f579251ddf994f819d83943ca47ab
Dineshraj Goomany <dineshraj.goomany@bbc.co.uk>
2018-03-10 18:55:43 +0000

    use fakeStore and add test for inital message

--- tests/lib/containers/calculator.jsx
+++ tests/lib/containers/calculator.jsx
@@ -11,45 +11,47 @@ import TickBox from '../../../lib/components/TickBox';
 
 import * as actions from '../../../lib/store/actions';
 
-const store = {
-  subscribe: function () {},
-  dispatch: function () {},
-  getState: function () {
-    return {
-      pokemonName: 'Pidgey',
-      candyCost: 12,
-      pokemonAmount: null,
-      candyAmount: null,
-      luckyEgg: false,
-      transfer: false,
-      error: false,
-      pokemon: [
-        ['Pidgey', '12']
-      ],
-      groups: [
-        {
-          'groupId': '12',
-          'title': '12 Candies'
-        }
-      ]
-    };
-  }
-};
+function getFakeStore(overrides) {
+  return {
+    subscribe: function () {},
+    dispatch: function () {},
+    getState: function () {
+      return Object.assign({
+        pokemonName: 'Pidgey',
+        candyCost: 12,
+        pokemonAmount: null,
+        candyAmount: null,
+        luckyEgg: false,
+        transfer: false,
+        error: false,
+        pokemon: [
+          ['Pidgey', '12']
+        ],
+        groups: [
+          {
+            'groupId': '12',
+            'title': '12 Candies'
+          }
+        ]
+      }, overrides);
+    }
+  };
+}
 
 describe('<Calculator />', () => {
 
   it('renders the Calculator correctly', () => {
-    const calculator = shallow(<Calculator store={store} />).dive();
+    const calculator = shallow(<Calculator store={getFakeStore()} />).dive();
     assert.equal(calculator.find('.calculator').length, 1);
   });
 
   it('renders the calculator element correctly', () => {
-    const calculator = shallow(<Calculator store={store} />).dive();
+    const calculator = shallow(<Calculator store={getFakeStore()} />).dive();
     assert.equal(calculator.find('form').length, 1);
   });
 
   it('renders the search component', () => {
-    const calculator = shallow(<Calculator store={store} />).dive();
+    const calculator = shallow(<Calculator store={getFakeStore()} />).dive();
     assert.equal(calculator.find(Search).length, 1);
   });
 
@@ -59,6 +61,7 @@ describe('<Calculator />', () => {
       label: 'Pidgey',
       value: '12'
     }];
+    const store = getFakeStore();
 
     const calculator = shallow(<Calculator store={store} />).dive();
 
@@ -67,27 +70,27 @@ describe('<Calculator />', () => {
   });
 
   it('passes in a chance callback into the search component', () => {
-    const calculator = shallow(<Calculator store={store} />).dive();
+    const calculator = shallow(<Calculator store={getFakeStore()} />).dive();
     assert.equal(typeof calculator.find(Search).prop('onChange'), 'function');
   });
 
   it('renders two message components', () => {
-    const calculator = shallow(<Calculator store={store} />).dive();
+    const calculator = shallow(<Calculator store={getFakeStore()} />).dive();
     assert.equal(calculator.find(Message).length, 2);
   });
 
   it('renders two entrybox components', () => {
-    const calculator = shallow(<Calculator store={store} />).dive();
+    const calculator = shallow(<Calculator store={getFakeStore()} />).dive();
     assert.equal(calculator.find(EntryBox).length, 2);
   });
 
   it.skip('renders two tickbox components', () => {
-    const calculator = shallow(<Calculator store={store} />).dive();
+    const calculator = shallow(<Calculator store={getFakeStore()} />).dive();
     assert.equal(calculator.find(TickBox).length, 2);
   });
 
   it('renders the reset input button', () => {
-    const calculator = shallow(<Calculator store={store} />).dive();
+    const calculator = shallow(<Calculator store={getFakeStore()} />).dive();
     assert.equal(calculator.find('.calculator__reset').length, 1);
   });
 
@@ -103,8 +106,20 @@ describe('<Calculator />', () => {
 
   it('calls the reset action when the reset button is clicked', () => {
     const resetSpy = sinon.spy(actions, 'reset');
-    const calculator = shallow(<Calculator store={store} />).dive();
+    const calculator = shallow(<Calculator store={getFakeStore()} />).dive();
     calculator.find('.calculator__reset').simulate('click');
     sinon.assert.calledOnce(resetSpy);
   });
+
+  it.only('displays the correct message if pokemonAmount and candyAmount is false', () => {
+    const calculator = shallow(<Calculator store={getFakeStore()} />).dive();
+    const message = calculator.find('.message--info');
+    const expectedText = 'Enter a Pokémon, the number of Pokémon you have and/or the number of Candy you have.';
+    assert.equal(message.text(), expectedText)
+  });
+
+  it('displays the updated message if pokemonAmount and candyAmount is have values', () => {
+
+  });
+
 });
