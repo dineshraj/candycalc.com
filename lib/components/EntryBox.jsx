87b3473a943e006d4681b5e827aaa720ff2c68d9
Dineshraj Goomany <dineshraj.goomany@bbc.co.uk>
2018-03-11 00:12:25 +0000

    min value of 0 for entry boxes

--- lib/components/EntryBox.jsx
+++ lib/components/EntryBox.jsx
@@ -9,7 +9,7 @@ export default ({ id, label, changeCallback }) => {
   return (
     <div className="entry-box">
       <Label id={id} label={label} />
-      <input className="entry-box__input" id={id} name={id} maxLength="3" autoComplete="off" type="number" onChange={(e) => _updateValue(e, changeCallback)} />
+      <input className="entry-box__input" id={id} name={id} maxLength="3" min="0" autoComplete="off" type="number" onChange={(e) => _updateValue(e, changeCallback)} />
     </div>
   );
 };
