5eb735f9a835e28e38a2a6b8caea75833e4c5c2e
Dineshraj Goomany <dineshraj.goomany@bbc.co.uk>
2018-03-11 00:12:45 +0000

    starts adding main app logic and tests

--- lib/containers/Calculator.jsx
+++ lib/containers/Calculator.jsx
@@ -12,6 +12,7 @@ import {
   setCandyAmount,
   setLuckyEgg,
   setTransfer,
+  setResults,
   reset
 } from '../store/actions/';
 
@@ -37,30 +38,88 @@ class Calculator extends React.Component {
 
 
   _renderMessage() {
-    return 'test'
+    const results = this.props.results;
+    const pokemonName = this.props.pokemonName
+    const pokemonAmount = this.props.pokemonAmount
+    const candyAmount = this.props.candyAmount
+
+    console.log(results);
+
+    if (pokemonAmount === 0 || pokemonAmount === null) {
+      return `You must have at least one ${pokemonName} if you want to evolve some!`
+    } else if (!results.canEvolve) {
+      return `You don't have enough Candy to evolve any of your ${pokemonName}, you need at least ${candyAmount}. Catch some more to get more Candy!`;
+    } else {
+      return 'bye';
+    }
+  }
+
+  _shouldCalculateResults(currentProps, nextProps) {
+    const props = ['pokemonName', 'candyCost', 'pokemonAmount', 'candyAmount', 'luckyEgg', 'transfer'];
+
+    return props.some((prop) => {
+      return currentProps[prop] !== nextProps[prop];
+    })
+  }
+
+  componentWillUpdate(nextProps) {
+    console.log('componentWillUpdate');
+    const currentProps = this.props;
+
+    console.log('should have specific message?', this._shouldCalculateResults(currentProps, nextProps));
+
+    if (this._shouldCalculateResults(currentProps, nextProps)) {
+      const canEvolve = (nextProps.pokemonAmount * nextProps.candyCost) <= nextProps.candyAmount;
+      // const numberCanEvolve = Math.floor(nextProps.candyAmount / nextProps.candyCost);
+      // const candyLeft = nextProps.candyAmount % nextProps.candyCost;
+
+      const results = {
+        canEvolve
+        // numberCanEvolve,
+        // candyLeft
+      };
+
+      this.props.resultsDispatch(results);
+    }
   }
 
   render() {
-    const pokemonArray = this._processPokemon(this.props.pokemon);
+    const {
+      pokemon,
+      groups,
+      pokemonAmount,
+      candyAmount,
+      luckyEgg,
+      transfer,
+      pokemonAmountDispatch,
+      candyAmountDispatch,
+      luckyEggDispatch,
+      transferDispatch,
+      resetDispatch
+    } = this.props;
+    const pokemonArray = this._processPokemon(pokemon);
+
     let message;
 
-    if (this.props.pokemonAmount || this.props.candyAmount) {
-      message = this._renderMessage();
+    if (pokemonAmount || candyAmount) {
+      message = this._renderMessage(luckyEgg, transfer);
     } else {
       message = 'Enter a Pokémon, the number of Pokémon you have and/or the number of Candy you have.';
     }
 
+    console.log(message);
+
     return (
       <div className="calculator">
         <form className="calculator__form">
-          <Search label="Choose a Pok&eacute;mon" groups={this.props.groups} pokemon={pokemonArray} onChange={(e) => this._updatePokemonState(e)}/>
-          <EntryBox id="pokemon" label="How many of these Pok&eacute;mon?" changeCallback={this.props.pokemonAmountDispatch} />
-          <EntryBox id="candy" label="How many of these candy?" changeCallback={this.props.candyAmountDispatch} />
-          <TickBox id="luckyEgg" label="Using a Lucky Egg?" isChecked={this.props.luckyEgg} clickCallback={this.props.luckyEggDispatch} />
-          <TickBox id="transfer" label="Transfer evolution?" isChecked={this.props.transfer} clickCallback={this.props.transferDispatch} />
+          <Search label="Choose a Pok&eacute;mon" groups={groups} pokemon={pokemonArray} onChange={(e) => this._updatePokemonState(e)}/>
+          <EntryBox id="pokemon" label="How many of these Pok&eacute;mon?" changeCallback={pokemonAmountDispatch} />
+          <EntryBox id="candy" label="How many of these candy?" changeCallback={candyAmountDispatch} />
+          <TickBox id="luckyEgg" label="Using a Lucky Egg?" isChecked={luckyEgg} clickCallback={luckyEggDispatch} />
+          <TickBox id="transfer" label="Transfer evolution?" isChecked={transfer} clickCallback={transferDispatch} />
           <Message message={message} overrideClass="message--info" />
           <Message message="You can evolve roughly 60 Pokémon in the 30 minutes a Lucky Egg is active for. So try and get as many candy as you can!" overrideClass="message--hint" />
-          <input type="reset" className="calculator__reset" value="Start Again" onClick={this.props.resetDispatch} />
+          <input type="reset" className="calculator__reset" value="Start Again" onClick={resetDispatch} />
         </form>
       </div>
     );
@@ -81,6 +140,7 @@ function mapDispatchToProps(dispatch) {
     candyAmountDispatch: (payload) => dispatch(setCandyAmount(payload)),
     luckyEggDispatch: (payload) => dispatch(setLuckyEgg(payload)),
     transferDispatch: (payload) => dispatch(setTransfer(payload)),
+    resultsDispatch: (payload) => dispatch(setResults(payload)),
     resetDispatch: () => dispatch(reset())
   };
 }
--- tests/lib/containers/calculator.jsx
+++ tests/lib/containers/calculator.jsx
@@ -1,4 +1,4 @@
-import assert from 'assert';
+import { assert } from 'chai';
 import { shallow, mount } from 'enzyme';
 import sinon from 'sinon';
 
@@ -8,92 +8,97 @@ import Search from '../../../lib/components/Search';
 import Message from '../../../lib/components/Message';
 import EntryBox from '../../../lib/components/EntryBox';
 import TickBox from '../../../lib/components/TickBox';
-
 import * as actions from '../../../lib/store/actions';
 
-function getFakeStore(overrides) {
-  return {
-    subscribe: function () {},
-    dispatch: function () {},
-    getState: function () {
-      return Object.assign({
-        pokemonName: 'Pidgey',
-        candyCost: 12,
-        pokemonAmount: null,
-        candyAmount: null,
-        luckyEgg: false,
-        transfer: false,
-        error: false,
-        pokemon: [
-          ['Pidgey', '12']
-        ],
-        groups: [
-          {
-            'groupId': '12',
-            'title': '12 Candies'
-          }
-        ]
-      }, overrides);
+import configureStore from 'redux-mock-store' //ES6 modules
+const middlewares = []
+const mockStore = configureStore();
+
+const initialState = {
+  pokemonName: 'Pidgey',
+  candyCost: 12,
+  pokemonAmount: null,
+  candyAmount: null,
+  luckyEgg: false,
+  transfer: false,
+  error: false,
+  results: {},
+  groups: [
+    {
+      groupId: "12",
+      title: "12 Candies"
+    }, {
+      groupId: "25",
+      title: "25 Candies"
     }
-  };
+  ],
+  pokemon: [
+    ["Pidgey", "12"],
+    ["Weedle", "12"]
+  ]
+};
+
+const store = getMockStore();
+
+function getMockStore(override = {}) {
+  return mockStore(
+    Object.assign(
+      {},
+      initialState,
+      override
+    )
+  )
 }
 
 describe('<Calculator />', () => {
 
 
   it('passes the processed app data into the search component', () => {
-    const expectedOptions = [{
-      groupId: '12',
-      label: 'Pidgey',
-      value: '12'
-    }];
-    const store = getFakeStore();
-
     const calculator = shallow(<Calculator store={store} />).dive();
 
     assert.deepEqual(calculator.find(Search).prop('groups'), store.getState().groups);
-    assert.deepEqual(calculator.find(Search).prop('pokemon'), expectedOptions);
+    assert.deepEqual(calculator.find(Search).prop('pokemon'), store.getState().pokemon);
   });
 
   it('passes in a change callback into the search component', () => {
-    const calculator = shallow(<Calculator store={getFakeStore()} />).dive();
+    const calculator = shallow(<Calculator store={store} />).dive();
     assert.equal(typeof calculator.find(Search).prop('onChange'), 'function');
   });
 
   describe('rendering', () => {
 
     it('renders the Calculator correctly', () => {
-      const calculator = shallow(<Calculator store={getFakeStore()} />).dive();
+      const calculator = shallow(<Calculator store={store} />).dive();
       assert.equal(calculator.find('.calculator').length, 1);
     });
 
     it('renders the calculator element correctly', () => {
-      const calculator = shallow(<Calculator store={getFakeStore()} />).dive();
+      const calculator = shallow(<Calculator store={store} />).dive();
       assert.equal(calculator.find('form').length, 1);
     });
 
     it('renders the search component', () => {
-      const calculator = shallow(<Calculator store={getFakeStore()} />).dive();
+      const calculator = shallow(<Calculator store={store} />).dive();
       assert.equal(calculator.find(Search).length, 1);
     });
 
     it('renders two message components', () => {
-      const calculator = shallow(<Calculator store={getFakeStore()} />).dive();
+      const calculator = shallow(<Calculator store={store} />).dive();
       assert.equal(calculator.find(Message).length, 2);
     });
 
     it('renders two entrybox components', () => {
-      const calculator = shallow(<Calculator store={getFakeStore()} />).dive();
+      const calculator = shallow(<Calculator store={store} />).dive();
       assert.equal(calculator.find(EntryBox).length, 2);
     });
 
     it('renders two tickbox components', () => {
-      const calculator = mount(<Calculator store={getFakeStore()} />);
+      const calculator = mount(<Calculator store={store} />);
       assert.equal(calculator.find('.tick-box').length, 2);
     });
 
     it('renders the reset input button', () => {
-      const calculator = shallow(<Calculator store={getFakeStore()} />).dive();
+      const calculator = shallow(<Calculator store={store} />).dive();
       assert.equal(calculator.find('.calculator__reset').length, 1);
     });
 
@@ -104,7 +109,7 @@ describe('<Calculator />', () => {
     it('calls the setPokemonName and setCandyCost action when the search input is changed', () => {
       const setPokemonNameSpy = sinon.spy(actions, 'setPokemonName');
       const setCandyCostSpy = sinon.spy(actions, 'setCandyCost');
-      const calculator = shallow(<Calculator store={getFakeStore()} />);
+      const calculator = shallow(<Calculator store={store} />);
       const search = calculator.dive().find(Search).simulate('change', { target: { value: 'Weedle' } } );
       sinon.assert.calledOnce(setPokemonNameSpy);
       sinon.assert.calledOnce(setCandyCostSpy);
@@ -112,53 +117,97 @@ describe('<Calculator />', () => {
 
     it('calls the setPokemonAmount action when the input is changed', () => {
       const setPokemonAmountSpy = sinon.spy(actions, 'setPokemonAmount');
-      const calculator = mount(<Calculator store={getFakeStore()} />);
+      const calculator = mount(<Calculator store={store} />);
       const search = calculator.find('.entry-box__input').at(0).simulate('change', { target: { value: 1 } } );
       sinon.assert.calledOnce(setPokemonAmountSpy);
     });
 
     it('calls the setCandyAmount action when the input is changed', () => {
       const setCandyAmountSpy = sinon.spy(actions, 'setCandyAmount');
-      const calculator = mount(<Calculator store={getFakeStore()} />);
+      const calculator = mount(<Calculator store={store} />);
       const search = calculator.find('.entry-box__input').at(1).simulate('change', { target: { value: 1 } } );
       sinon.assert.calledOnce(setCandyAmountSpy);
     });
 
     it('calls the setLuckyEgg action when the checkbox is clicked', () => {
       const setLuckyEggSpy = sinon.spy(actions, 'setLuckyEgg');
-      const calculator = mount(<Calculator store={getFakeStore()} />);
+      const calculator = mount(<Calculator store={store} />);
       const search = calculator.find('.tick-box__input').at(0).simulate('change');
       sinon.assert.calledOnce(setLuckyEggSpy);
     });
 
     it('calls the setTransfer action when the checkbox is clicked', () => {
       const setTransferSpy = sinon.spy(actions, 'setTransfer');
-      const calculator = mount(<Calculator store={getFakeStore()} />);
+      const calculator = mount(<Calculator store={store} />);
       const search = calculator.find('.tick-box__input').at(1).simulate('change');
       sinon.assert.calledOnce(setTransferSpy);
     });
 
     it('calls the reset action when the reset button is clicked', () => {
       const resetSpy = sinon.spy(actions, 'reset');
-      const calculator = shallow(<Calculator store={getFakeStore()} />).dive();
+      const calculator = shallow(<Calculator store={store} />).dive();
       calculator.find('.calculator__reset').simulate('click');
       sinon.assert.calledOnce(resetSpy);
     });
 
   });
 
-  it('displays the correct message if pokemonAmount and candyAmount is false', () => {
-    const calculator = shallow(<Calculator store={getFakeStore()} />);
-    const message = calculator.dive().find(Message).get(0).props.message;
-    const expectedText = 'Enter a Pokémon, the number of Pokémon you have and/or the number of Candy you have.';
-    assert.equal(message, expectedText)
-  });
+  describe.only('calculator logic', () => {
+    afterEach(() => {
 
-  it('displays the updated message if pokemonAmount and candyAmount have values', () => {
-    const calculator = shallow(<Calculator store={getFakeStore({ pokemonAmount: 2, candyAmount: 2 })} />);
-    const message = calculator.dive().find(Message).get(0).props.message;
-    const expectedText = 'test';
-    assert.equal(message, expectedText)
+    });
+
+    it('displays the default message if pokemonAmount and candyAmount is false', () => {
+      const calculator = shallow(<Calculator store={store} />);
+      const message = calculator.dive().find(Message).get(0).props.message;
+      const defaultText = 'Enter a Pokémon, the number of Pokémon you have and/or the number of Candy you have.';
+      assert.equal(message, defaultText)
+    });
+
+    it('does not display default message if pokemonAmount has a value', () => {
+      const store = getMockStore({ pokemonAmount: 1 });
+      const calculator = mount(<Calculator store={store} />);
+      const message = calculator.find(Message).get(0).props.message;
+      const defaultText = 'Enter a Pokémon, the number of Pokémon you have and/or the number of Candy you have.';
+      assert.notEqual(message, defaultText);
+    });
+
+    it('does not display default message if candyAmount has a value', () => {
+      const store = getMockStore({ candyAmount: 2 });
+      const calculator = shallow(<Calculator store={store} />);
+      const message = calculator.dive().find(Message).get(0).props.message;
+      const defaultText = 'Enter a Pokémon, the number of Pokémon you have and/or the number of Candy you have.';
+      assert.notEqual(message, defaultText)
+    });
+
+    it('displays the correct message if you have none of the chosen pokemon', () => {
+      const store = getMockStore({ pokemonAmount: 0, candyAmount: 24 });
+      const calculator = shallow(<Calculator store={store} />);
+      const message = calculator.dive().find(Message).get(0).props.message;
+      const expectedText = "You must have at least one Pidgey if you want to evolve some!";
+      assert.equal(message, expectedText)
+    });
+
+    it('displays correct message if you do not have enough candy to evolve your pokemon', () => {
+      const store = getMockStore({ pokemonAmount: 1, candyAmount: 1 });
+      const calculator = shallow(<Calculator store={store} />);
+      const message = calculator.dive().find(Message).get(0).props.message;
+      const expectedText = "You don't have enough Candy to evolve any of your Pidgey, you need at least 12. Catch some more to get more Candy!";
+      assert.equal(message, expectedText)
+    });
+
+    it.only('calculates if you can evolve all your pokemon and how much candy is left', () => {
+      const store = getMockStore({ pokemonAmount: 1, candyAmount: 14 });
+      const calculator = mount(<Calculator store={store} />);
+      const message = calculator.find(Message).get(0).props.message;
+      const expectedText = "You can evolve all your Pidgey using your 28 Candy. You'll have 16 Candy left over, better go get some more!";
+      assert.include(message, expectedText)
+    });
+
+    it('calculates how many candy you will have left over');
+    it('calculates how many candy you will have left over when also transferring');
+    it('calculates XP you will earn');
+    it('calculates XP you will earn with a lucky egg');
   });
 
 });
