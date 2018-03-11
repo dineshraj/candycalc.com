876e604fc6b0df6ba0d418bd9cf50420f7f3aff6
Dineshraj Goomany <dineshraj.goomany@bbc.co.uk>
2018-03-11 09:34:54 +0000

    parameterises ad-client

--- lib/containers/App.jsx
+++ lib/containers/App.jsx
@@ -4,13 +4,15 @@ import Header from '../components/Header';
 import Ad from '../components/Ad';
 import Calculator from '../containers/Calculator';
 
+const AD_CLIENT = 'ca-pub-9464301444690248';
+
 export function App() {
   return (
     <div>
       <Header />
-      <Ad client="ca-pub-9464301444690248" slot="1890101450" />
+      <Ad client={AD_CLIENT} slot="1890101450" />
       <Calculator />
-      <Ad client="ca-pub-9464301444690248" slot="3223573855" />
+      <Ad client={AD_CLIENT} slot="3223573855" />
     </div>
   );
 }
