9be049a1202328ea230a3879773e40713f17521d
Dineshraj Goomany <dineshraj.goomany@bbc.co.uk>
2018-03-29 14:45:22 +0100

    listen for port ENV var

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ import { Provider } from 'react-redux';
 import configureStore from './lib/store/configureStore';
 import * as AppMain from './lib/containers/App';
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 const app = express();
 const store = configureStore();
 
